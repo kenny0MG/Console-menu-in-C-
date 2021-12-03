06d00cbaf6e8a56441eedeb418579e93b3936520
kenny0MG <staspricepa144@gmail.com>
2021-12-03 19:48:26 +0300

    Update StasSystem.cs

--- StasSystem.cs
+++ StasSystem.cs
@@ -30,11 +30,11 @@ namespace ConsoleApp8
             for (int i = 0; i < Options.Length; i++)
             {
                 string currentOption = Options[i];
-                string prefix;
+                string prefix = "*";
                 if (i == SelectedIndex)
                 {
-                    
-                    prefix = " = ";
+
+                    prefix = " * ";
                     ForegroundColor = ConsoleColor.Black;
                     BackgroundColor = ConsoleColor.Green;
 
@@ -86,11 +86,19 @@ namespace ConsoleApp8
 
     class System//само приложение
     {
-        static void Main(string[] args)
 
+        static void Main(string[] args)
         {
-            ForegroundColor = ConsoleColor.Red;
-            string prompt="Добро пожаловать!!!";
+
+
+
+
+
+
+            string prompt = @"█░░░█ █▀▀ █░░ ▄▀ ▄▀▄ █▄░▄█ █▀▀ 
+█░█░█ █▀▀ █░▄ █░ █░█ █░█░█ █▀▀ 
+░▀░▀░ ▀▀▀ ▀▀▀ ░▀ ░▀░ ▀░░░▀ ▀▀▀ 
+Добро пожаловать в систему!!!";
 
 
             string[] options = { "Войти", "Зарегистрироваться", "Выйти" };//массив значений в options
@@ -98,11 +106,315 @@ namespace ConsoleApp8
             Menu MainMenu = new Menu(options, prompt);
             int selectedIndex = MainMenu.Run();
 
-            //WriteLine("Нажмите на любую кнопку для выхода...");
-            // ReadKey(true);
 
-            ResetColor();
+            switch (selectedIndex)
+            {
+                case 0:
+                    User();
+                    break;
+                case 1:
+                    NewUser();
+                    break;
+                case 2:
+                    ExitSystem();
+                    break;
+
+            }
+
+            void ExitSystem()//система выхода
+            {
+                WriteLine("Если хотите вернуться в меню нажмите клавишу esc");
+                WriteLine("\nНажмите дважды на любую кнопку для выхода");
+                ConsoleKey keyPressed;
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    Main(args);
+                }
+                ReadKey(true);
+                Environment.Exit(0);
+            }
+            void NewUser()//система регистрации с возможностью возвращения в меню
+            {
+
+                Clear();
+                WriteLine("Если хотите вернуться в меню нажмите ecs");
+                WriteLine("\nЧто бы зарегестрироваться введите логин и пароль:");
+
+                ConsoleKey keyPressed;
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    Main(args);
+                }
+                ReadKey();
+
+            }
+            void User( )//вход в систему уже зарегестривовшегося пользователя
+            {
+            m:
+                WriteLine("Если хотите вернуться в меню нажмите esc");
+                
+                string a,b,c,d,e,login;//присваиваю значение логинов
+                a = "Admin";
+                b = "HR";
+                c = "Sklad";
+                d = "Buhgalter";
+                e = "Manager";
+                
+                WriteLine("Введите логин");
+                ConsoleKey keyPressed;
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    Main(args);
+                }
+               
+
+                login = ReadLine();
+                if (login == a)//проверка на логин
+                {
+                    string password = "Kenny0MG4", inpt = string.Empty;
+                    while (password != inpt)//проверка на пароль
+                    {
+                        Console.Write("Введите пароль для работы с файлами Windows: ");
+                        inpt = string.Empty;
+                        while (true)
+                        {
+                            var key = Console.ReadKey(true);//не отображаем клавишу - true
+
+                            if (key.Key == ConsoleKey.Enter) break; //enter - выходим из цикла
+
+                            Console.Write("*");//рисуем звезду вместо нее
+                            inpt += key.KeyChar; //копим в пароль символы
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.Write("Допуск получен!");
+                    Admin();
+                    Console.ReadKey();
+
+                } //поиск по логину и проверка пароля           
+                if (login == b)
+                {
+                    string password = "qwerty123", inpt = string.Empty;
+                    while (password != inpt)
+                    {
+                        Console.Write("Введите пароль для работы с файлами Windows: ");
+                        inpt = string.Empty;
+                        while (true)
+                        {
+                            var key = Console.ReadKey(true);//не отображаем клавишу - true
+
+                            if (key.Key == ConsoleKey.Enter) break; //enter - выходим из цикла
+
+                            Console.Write("*");//рисуем звезду вместо нее
+                            inpt += key.KeyChar; //копим в пароль символы
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.Write("Допуск получен!");
+                    HRMenedj();
+                    Console.ReadKey();
+
+                }
+                if (login == c)
+                {
+                    string password = "Salam11", inpt = string.Empty;
+                    while (password != inpt)
+                    {
+                        Console.Write("Введите пароль для работы с файлами Windows: ");
+                        inpt = string.Empty;
+                        while (true)
+                        {
+                            var key = Console.ReadKey(true);//не отображаем клавишу - true
+
+                            if (key.Key == ConsoleKey.Enter) break; //enter - выходим из цикла
+
+                            Console.Write("*");//рисуем звезду вместо нее
+                            inpt += key.KeyChar; //копим в пароль символы
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.Write("Допуск получен!");
+                    Sklad();
+                    Console.ReadKey();
+
+                }
+                if (login == d)
+                {
+                    string password = "228666Ster", inpt = string.Empty;
+                    while (password != inpt)
+                    {
+                        Console.Write("Введите пароль для работы с файлами Windows: ");
+                        inpt = string.Empty;
+                        while (true)
+                        {
+                            var key = Console.ReadKey(true);//не отображаем клавишу - true
+
+                            if (key.Key == ConsoleKey.Enter) break; //enter - выходим из цикла
+
+                            Console.Write("*");//рисуем звезду вместо нее
+                            inpt += key.KeyChar; //копим в пароль символы
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.Write("Допуск получен!");
+                    Buhgalter();
+                    Console.ReadKey();
+
+                }
+                if (login == e)
+                {
+                    string password = "1488Dima", inpt = string.Empty;
+                    while (password != inpt)
+                    {
+                        Console.Write("Введите пароль для работы с файлами Windows: ");
+                        inpt = string.Empty;
+                        while (true)
+                        {
+                            var key = Console.ReadKey(true);//не отображаем клавишу - true
+
+                            if (key.Key == ConsoleKey.Enter) break; //enter - выходим из цикла
+
+                            Console.Write("*");//рисуем звезду вместо нее
+                            inpt += key.KeyChar; //копим в пароль символы
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.Write("Допуск получен!");
+                    Manager();
+                    Console.ReadKey();
+
+                }
+
+
+
+                else
+                {
+                    WriteLine("Повторите логин");
+                    goto m;
+                }
+            }
+            void Admin()//панель каждого рабочего
+            {
+                Clear();
+                ForegroundColor = ConsoleColor.Yellow;
+                WriteLine(@"╔═══╗╔═══╗╔═╗╔═╗╔══╗╔═╗─╔╗
+║╔═╗║╚╗╔╗║║║╚╝║║╚╣─╝║║╚╗║║
+║║─║║─║║║║║╔╗╔╗║─║║─║╔╗╚╝║
+║╚═╝║─║║║║║║║║║║─║║─║║╚╗║║
+║╔═╗║╔╝╚╝║║║║║║║╔╣─╗║║─║║║
+╚╝─╚╝╚═══╝╚╝╚╝╚╝╚══╝╚╝─╚═╝
+<<ВЫ АДМИН>>если хотите вернуться в меню нажмите esc");
+                ConsoleKey keyPressed;
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    Main(args);
+                }
+                ResetColor();
+                ReadKey(true);
+            }//рабочая панель каждого рабочего
+            void HRMenedj()
+            {
+                Clear();
+                ForegroundColor = ConsoleColor.Blue;
+                WriteLine(@"╔╗─╔╗╔═══╗
+║║─║║║╔═╗║
+║╚═╝║║╚═╝║
+║╔═╗║║╔╗╔╝
+║║─║║║║║╚╗
+╚╝─╚╝╚╝╚═╝
+<<ВЫ HR-МЕНЕДЖЕР>>если хотите вернуться в меню нажмите esc");
+                ConsoleKey keyPressed;
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    Main(args);
+                }
+                ResetColor();
+                ReadKey(true);
+            }
+            void Sklad()
+            {
+                Clear();
+                ForegroundColor = ConsoleColor.Cyan;
+                WriteLine(@"╔═══╗╔═══╗╔═══╗╔═══╗╔═══╗╔════╗╔═══╗╔═══╗     ╔═══╗╔╗╔═╗╔╗───╔═══╗╔═══╗╔═══╗
+║╔═╗║║╔═╗║║╔══╝║╔═╗║║╔═╗║║╔╗╔╗║║╔═╗║║╔═╗║     ║╔═╗║║║║╔╝║║───║╔═╗║╚╗╔╗║║╔═╗║
+║║─║║║╚═╝║║╚══╗║╚═╝║║║─║║╚╝║║╚╝║║─║║║╚═╝║     ║╚══╗║╚╝╝─║║───║║─║║─║║║║║║─║║
+║║─║║║╔══╝║╔══╝║╔╗╔╝║╚═╝║──║║──║║─║║║╔╗╔╝     ╚══╗║║╔╗║─║║─╔╗║╚═╝║─║║║║║╚═╝║
+║╚═╝║║║───║╚══╗║║║╚╗║╔═╗║──║║──║╚═╝║║║║╚╗     ║╚═╝║║║║╚╗║╚═╝║║╔═╗║╔╝╚╝║║╔═╗║
+╚═══╝╚╝───╚═══╝╚╝╚═╝╚╝─╚╝──╚╝──╚═══╝╚╝╚═╝     ╚═══╝╚╝╚═╝╚═══╝╚╝─╚╝╚═══╝╚╝─╚╝
+<<ВЫ ОПЕРАТОР СКЛАДА>>если хотите вернуться в меню нажмите esc");
+                ConsoleKey keyPressed;
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    Main(args);
+                }
+                ResetColor();
+                ReadKey(true);
+            }
+            void Buhgalter()
+            {
+                Clear();
+                ForegroundColor = ConsoleColor.Magenta;
+                WriteLine(@"╔══╗─╔╗─╔╗╔╗─╔╗╔═══╗╔═══╗╔╗───╔════╗╔═══╗╔═══╗
+║╔╗║─║║─║║║║─║║║╔═╗║║╔═╗║║║───║╔╗╔╗║║╔══╝║╔═╗║
+║╚╝╚╗║║─║║║╚═╝║║║─╚╝║║─║║║║───╚╝║║╚╝║╚══╗║╚═╝║
+║╔═╗║║║─║║║╔═╗║║║╔═╗║╚═╝║║║─╔╗──║║──║╔══╝║╔╗╔╝
+║╚═╝║║╚═╝║║║─║║║╚╩═║║╔═╗║║╚═╝║──║║──║╚══╗║║║╚╗
+╚═══╝╚═══╝╚╝─╚╝╚═══╝╚╝─╚╝╚═══╝──╚╝──╚═══╝╚╝╚═╝
+<<ВЫ БУХГАЛТЕР>>если хотите вернуться в меню нажмите esc");
+                ConsoleKey keyPressed;
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    Main(args);
+                }
+                ResetColor();
+                ReadKey(true);
+            }
+            void Manager()
+            {
+                Clear();
+                ForegroundColor = ConsoleColor.DarkYellow;
+                WriteLine(@"╔═╗╔═╗╔═══╗╔═╗─╔╗╔═══╗╔═══╗╔═══╗╔═══╗
+║║╚╝║║║╔═╗║║║╚╗║║║╔═╗║║╔═╗║║╔══╝║╔═╗║
+║╔╗╔╗║║║─║║║╔╗╚╝║║║─║║║║─╚╝║╚══╗║╚═╝║
+║║║║║║║╚═╝║║║╚╗║║║╚═╝║║║╔═╗║╔══╝║╔╗╔╝
+║║║║║║║╔═╗║║║─║║║║╔═╗║║╚╩═║║╚══╗║║║╚╗
+╚╝╚╝╚╝╚╝─╚╝╚╝─╚═╝╚╝─╚╝╚═══╝╚═══╝╚╝╚═╝
+<<ВЫ МЕНЕДЖЕР>>если хотите вернуться в меню нажмите esc");
+                ConsoleKey keyPressed;
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    Main(args);
+                }
+                ResetColor();
+                ReadKey(true);
+
+            }
+
+
         }
 
+
+
     }
+    
+        
+
 }
+
+
